cbf806b62588597ff228c568da020afe628d53bd
v3ry <v3ryfr@gmail.com>
2022-05-30 10:59:43 +0200

    Shoot projectiles from the player

--- index.js
+++ index.js
@@ -34,5 +34,55 @@ class Player extends Entity{
     }
 }
 
+class Projectile extends Player {
+    constructor(x,y,radius,color,velocity){
+        super(x,y,radius,color);
+        this.velocity = velocity;
+    }
+    update(){
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
+
 const player = new Player(canvas.width/2,canvas.height/2,10,"red");
+const projectile = new Projectile(50,50,30,'blue',{x:3,y:3});
+const projectiles = [];
+
 player.draw();
+projectile.draw();
+
+
+window.addEventListener("click",(event)=>{
+    const angle = Math.atan2(
+        event.clientY - player.y,
+        event.clientX - player.x
+    );
+    const velocity = {
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5,
+    };
+    const projectile = new Projectile(
+        player.x,
+        player.y,
+        5,
+        "white",
+        velocity);
+    projectiles.push(projectile);
+    projectile.draw();
+    
+});
+
+function animate(){
+    requestAnimationFrame(animate);
+
+    ctx.fillStyle = "rgba(0,0,0,1)";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    player.draw();
+
+    projectiles.forEach((projectile) => projectile.update());
+}
+animate()
